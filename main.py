51d9dcffb181b773e8ce6a0e05b757dbba8b43fc
Maksim <maskimangerman@gmail.com>
2022-07-15 15:03:48 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from openpyxl import load_workbook
+
+wb1 = load_workbook('2017.xls')
+wb2 = load_workbook('2018.xls')
+wb3 = load_workbook('2019.xls')
+wb4 = load_workbook('2020.xls')
+wb5 = load_workbook('2021.xls')
+wb6 = load_workbook('2022.xls')
+print('h')
